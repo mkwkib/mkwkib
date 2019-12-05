3c7ca2fd449f5bafff8a51365fe9dc2857ffe783
cecepsprd21 <cecepsprd21@gmail.com>
2019-12-05 14:21:57 +0700

    ceceps

--- profil/koneksi.php
+++ profil/koneksi.php
@@ -2,6 +2,6 @@
 $conn = mysqli_connect('localhost','root','','profil');
 
 if (!$conn){
-    die("Connection Failed: "  . mysqli_connect_error());
+    die("Error 500 : "  . mysqli_connect_error());
 }
 ?>
