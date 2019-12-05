ee267ec622cfe7d237cff1f0c900321444800bf4
btpnsshifted <btpnsshifted@H0001N593605F2.corp.btpns.com>
2019-12-05 14:33:52 +0700

    ubah ini

--- profil/koneksi.php
+++ profil/koneksi.php
@@ -2,14 +2,9 @@
 $conn = mysqli_connect('localhost','root','','profil');
 
 if (!$conn){
-<<<<<<< HEAD
     die("Connection Failed: "  . mysqli_connect_error());
 }
 else {
     ("tesssss");
 }
-=======
-    die("Error 404 Not Found"  . mysqli_connect_error());
- }
->>>>>>> 33b0e21bd82857f49cd2cc280d48afeaa7bfa5cf
 ?>
